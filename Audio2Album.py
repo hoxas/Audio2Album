9c4f83febffd29a0282e9162f7170f1d4ec4ad46
Hoxas <hoxas@live.com>
2021-07-28 23:14:56 -0300

    Refactor and adding txt file functionality

--- Audio2Album.py
+++ Audio2Album.py
@@ -1,49 +1,21 @@
 import os, sys
 
 from datetime import datetime
-import time
+
+from pathlib import Path
+
+## FFMPEG ON PATH
 
 from mutagen.mp3 import MP3
 from mutagen import easyid3 as eid
 from pydub import AudioSegment
+from pydub.utils import mediainfo
 
-print(eid.EasyID3.valid_keys.keys())
+# print(eid.EasyID3.valid_keys.keys())
 
-try:
-    filename = sys.argv[1]
-except:
-    print('Enter file: ')
-    filename = input()
-
-if os.path.isabs(filename):
-    print('True')
-    pathfile = filename
-else:
-    print('False')
-    pathfile = os.path.join(os.getcwd(), filename)
-
-print(filename)
-
-file = AudioSegment.from_mp3(pathfile)
-
-totalt = MP3(pathfile).info.length
-
-print(totalt)
-
-print('AlbName: ')
-album = input()
-
-print('AlbArtist: ')
-artist = input()
-
-print('Date: ')
-release = input()
-
-print('How many trks: ')
-tracksnumber = int(input())
-
+# from string to datetime
 def getTime(t):
-    if t.upper() == 'END':
+    if str(t).upper() == 'END':
         return 'END'
 
     if '.' in t:
@@ -60,6 +32,7 @@ def getTime(t):
             t = datetime.strptime(t, f'%S{mili}')
     return t
 
+# to milliseconds
 def toMilli(t):
     if t == 'END':
         return totalt * 1000
@@ -68,23 +41,40 @@ def toMilli(t):
     seconds = t_timedelta.total_seconds()
     return seconds * 1000
 
-track = 1
+# clean up paths
+def getPath(filename):
+    if os.path.isabs(filename):
+        pathfile = filename
+        return pathfile
+    else:
+        if '.\\' in filename[0:2]:
+            filename = filename[2:] 
+        pathfile = os.path.join(os.getcwd(), filename)
+        return pathfile
 
-while track <= tracksnumber:
-    print('Track: ')
-    tname = input()
+# create txt file for easier use
+if sys.argv[1] == 'mktxt':
+    try:
+        name = getPath(sys.argv[2]) + '.txt'
+        w = open(name, 'w')
+    except:
+        w = open(getPath('ToAlbumStamps.txt'), 'w')
 
-    # get times
-    print('Start (h:m:s): ')
-    tstart = getTime(input())
-    mstart = toMilli(tstart)
+    lines = [
+        'Album:\n', 
+        'Artist:\n', 
+        'Release:\n',  
+        'VVV Tracks Below VVV\n',
+        '1.Track--Start--End\n',
+        '2.\n'
+        ]
+    for line in lines:
+        w.write(line)
+    exit()
 
-    print('End (h:m:s): ')
-    tend = getTime(input())
-    mend = toMilli(tend)
+category = {}
 
-    print(mstart, mend)
-
+def sliceMain():
     # cut track 
     cuttrack = file[mstart:mend]
 
@@ -97,20 +87,104 @@ while track <= tracksnumber:
 
     trackname = f"{zero}{track}. {tname}.mp3"
     
-    cuttrack.export(trackname, format='mp3')
+    cuttrack.export(trackname, format='mp3', bitrate=tbitrate)
 
     # tagging 
     exported = eid.EasyID3(trackname)
 
     exported['tracknumber'] = str(track)
     exported['title'] = tname
-    exported['artist'] = artist
-    exported['albumartist'] = artist
-    exported['album'] = album
-    exported['date'] = release
+    exported['artist'] = category['artist']
+    exported['albumartist'] = category['artist']
+    exported['album'] = category['album']
+    exported['date'] = category['release']
     exported.save()
 
-    track += 1
+### Main Program Start
+
+# Test for given filepath/name
+try:
+    filename = sys.argv[1]
+except:
+    print('Enter file: ')
+    filename = input()
+
+# get and filter path
+pathfile = getPath(filename)
+
+# getting input bitrate
+tbitrate = mediainfo(pathfile)['bit_rate']
+# opening file
+file = AudioSegment.from_mp3(pathfile)
+# get total length (unused still)
+totalt = MP3(pathfile).info.length
+print(totalt)
+
+### IF TXT FILE PATH GIVEN
+if len(sys.argv) == 3:
+    txtpath = getPath(sys.argv[2])
+    # check if txt is there
+    if os.path.isfile(txtpath):
+        with open(txtpath, 'r') as r:
+            content = r.readlines()
+            isTrack = False;
+            for line in content:
+                # Getting tracktime and cutting
+                if isTrack == True:
+                    x = line.split('.')
+                    track = int(x[0])
+                    y = x[1].split('--')
+                    tname = y[0].strip()
+
+                    tstart = getTime(y[1].strip())
+                    mstart = toMilli(tstart)
+                    tend = getTime(y[2].strip())
+                    mend = toMilli(tend)
+
+                    sliceMain()
+                
+                # Activates track condition above
+                if 'VVV' in line:
+                    isTrack = True
+                
+                #Split Tagname from Name
+                if isTrack == False:
+                    x = line.split(':', 1)
+                    category[x[0].lower()] = x[1].replace('\n', '').strip()
+else:
+    print('AlbName: ')
+    category['album'] = input()
+
+    print('AlbArtist: ')
+    category['artist'] = input()
+
+    print('Date: ')
+    category['release'] = input()
+
+    print('How many trks: ')
+    numberoftracks = int(input())
+
+    track = 1
+
+    # Getting times by input
+    while track <= numberoftracks:
+        print('Track: ')
+        tname = input()
+
+        # get times
+        print('Start (h:m:s): ')
+        tstart = getTime(input())
+        mstart = toMilli(tstart)
+
+        print('End (h:m:s): ')
+        tend = getTime(input())
+        mend = toMilli(tend)
+
+        print(mstart, mend)
+
+        sliceMain()
+
+        track += 1
 
     
 
